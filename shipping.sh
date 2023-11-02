04de535d0c8ef577fbe545e63db57b1f5e86a3bf
SaiLakshmi-github <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-11-02 23:36:46 +0530

    shipping edit

--- shipping.sh
+++ shipping.sh
@@ -55,7 +55,7 @@ mv target/shipping-1.0.jar shipping.jar &>>$LOGFILE
 
 VALIDATE $? "renaming shipping jar"
 
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
+cp /home/centos/roboshop-shellscript/shipping.service /etc/systemd/system/shipping.service &>>$LOGFILE
 
 VALIDATE $? "copying shipping service"
 
