cfd9d615336d5943f10388d58165c90226ac8a78
SaiLakshmi-github <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-11-12 15:33:55 +0530

    adding servers

--- Ec2.sh
+++ Ec2.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 Names=("web" "cart" "mongodb" "mysql" "redis" "user" "shipping" "catalogue" "rabbitmq" "payment" "dispatch")
-
+Instance_Type=""
+Image_id=ami-03265a0778a880afb
+Security_id=sg-09fed927cbd854100
 for i in "${Names[@]}"
 do 
-echo "Name:$i"
-done
+if [ [$i == "mongodb" || $i == "mysql" ] ]
+then
+Instance_Type="t3.medium"
+else
+Instance_Type="t2.micro"
+fi
+echo "creating $i instance"
+aws ec2 run-instances --image-id $Image_id --instance-type t2.micro --security-group-ids $Security_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
