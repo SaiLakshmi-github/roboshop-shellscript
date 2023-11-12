89cb5b57d92d18a6612863e3222515971be1db85
SaiLakshmi-github <142158089+SaiLakshmi-github@users.noreply.github.com>
2023-11-12 15:51:01 +0530

    adding servers

--- Ec2.sh
+++ Ec2.sh
@@ -6,12 +6,13 @@ Image_id=ami-03265a0778a880afb
 Security_id=sg-09fed927cbd854100
 for i in "${Names[@]}"
 do 
-if [ [$i == "mongodb" || $i == "mysql" ] ]
+if [[$i == "mongodb" || $i == "mysql" ]]
 then
 Instance_Type="t3.medium"
 else
 Instance_Type="t2.micro"
 fi
 echo "creating $i instance"
-aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Security_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+IP_address=$(aws ec2 run-instances --image-id $Image_id --instance-type $Instance_Type --security-group-ids $Security_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+echo "created $i instance: $IP_address"
 done
